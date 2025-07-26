d875c4652540a0002ce9a430744f4edd60af864f
mohammadibrhaim273 <smohammadibrahim118@gmail.com>
2025-07-26 14:46:36 +0530

    GIT Reapply "add"
    This reverts commit 0e4fc0c7866fd8cba7dd14e6c689b29a029ac521.

--- git.java
+++ git.java
@@ -2,6 +2,7 @@ public class git {
     public static void main(String[] args) {
         System.out.println("ibrahim");
         System.out.println("ibrahim");
+        System.out.println("iraj");
     }
     
 }
